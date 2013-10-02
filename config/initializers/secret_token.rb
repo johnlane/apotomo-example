d81d1c1827a13538d96c48cf1299698f8f9c70ba
John Lane <john@lane.uk.net>
2013-10-02 13:47:25 +0100

    new rails4 app for apotomo tutorial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ApotomoTweet::Application.config.secret_key_base = 'e892542f95f007ac16d630d0b20eb627276293d861803bcf042e5d38265f03bd1c6891a73182e047b6395d1f46bcbe5354a9170e15f79048d3158970017127ff'
