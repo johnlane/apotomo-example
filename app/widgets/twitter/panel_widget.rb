08b731bb0457b9b96cb303b848d8799d69b1a18f
John Lane <johnlane.github@jelmail.com>
2013-10-03 10:32:31 +0100

    bubbling events

--- app/widgets/twitter/panel_widget.rb
+++ app/widgets/twitter/panel_widget.rb
@@ -1,4 +1,5 @@
 class Twitter::PanelWidget < Apotomo::Widget
+  responds_to_event :newTweet, :with => :redraw_list
    
   has_widgets do
     self << widget("twitter/form", :tweet_form)
@@ -12,6 +13,10 @@ class Twitter::PanelWidget < Apotomo::Widget
   
   def list(tweets)
     render :locals => {:tweets => tweets}
+  end
+
+  def redraw_list(evt)
+    replace "##{widget_id} ul", {:state => :list}, Tweet.all
   end
 
 end
